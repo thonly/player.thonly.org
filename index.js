ad3108038e6dfa365506fefa334215178ad06d00
Thon Ly <thonly@heartbank.ceo>
2022-07-16 20:52:11 -0700

    v1 complete

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
+const img = document.querySelector('img');
+const audio = document.querySelector('audio');
+audio.loop = true;
+const categories = ['THonly', 'Sisamuth', 'Anime', 'Pop', 'Foreign'];
+
 window.onload = async () => {
-    const response = await fetch('https://dns.thonly.net:432/');
-    const music = await response.json();
-    console.log(music)
+    const library = JSON.parse(localStorage.getItem('library'));
+    img.src = library[categories[0]][0].coverart;
+    img.onclick = () => window.open(`https://www.youtube.com/watch?v=${library[categories[0]][0].id}`, '_blank');
+    audio.src = library[categories[0]][0].url;
+    renderMusicLibrary(library);
+}
+
+window.getMusicLibrary = async () => {
+    const library = await fetch('https://dns.thonly.net:432/');
+    localStorage.setItem('library', JSON.stringify(await library.json()));
+    document.location.reload();
 }
 
 window.addMusic = async event => {
     event.preventDefault();
-    const response = await fetch('https://dns.thonly.net:432/', {
+    const library = await fetch('https://dns.thonly.net:432/', {
         method: 'POST', 
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(Object.fromEntries(new FormData(event.target)))
+    });    
+    localStorage.setItem('library', JSON.stringify(await library.json()));
+    document.location.reload();
+}
+
+function renderMusicLibrary(library) {
+    const nav = document.querySelector('nav');
+
+    categories.forEach(category => {
+        const h3 = document.createElement('h3');
+        h3.textContent = category;
+        const menu = document.createElement('menu');
+        nav.append(h3, menu);
+
+        library[category].forEach(song => {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+            img.src = song.thumbnail;
+            li.append(img, song.title);
+            li.onclick = () => playMusic(song);
+            menu.append(li);
+        });
     });
-    const status = await response.json();
-    console.log(status)
+}
+
+function playMusic(song) {
+    audio.src = song.url;
+    audio.play();
 }
 
 window.dataLayer = window.dataLayer || [];
--- index.mjs
+++ index.mjs
@@ -14,44 +14,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.json(getMusic());
+    res.json(getLibrary());
 });
 
-app.post('/', (req, res) => {
+// "liveServer.settings.ignoreFiles":["music/**"]
+app.post('/', async (req, res) => {
+    const title = await getInfo(req.body.videoID);
     const stream = ytdl(req.body.videoID, { filter: 'audioonly' });
     //stream.on('info', (info, format) => console.log(info, format));
     stream.on('progress', (chunkLength, downloaded, total) => console.log("Progress:", Math.floor((downloaded / total) * 100)));
-    stream.pipe(fs.createWriteStream(`music/${req.body.category}/${req.body.videoID}.webm`));
-    res.json({ status: 'success' });
+    stream.pipe(fs.createWriteStream(`music/${req.body.category}/${req.body.videoID} *|* ${title}.webm`).on('finish', () => res.json(getLibrary())));
 });
 
 https.createServer({cert, key}, app).listen(432);
 
-// async function getInfo(url) {
-//     let info = await ytdl.getInfo(url);
-//     let audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
-//     console.log('Formats with only audio: ' + JSON.stringify(audioFormats[0]));
-// }
-
-// getInfo('Us-2cMZu0kY');
-
-function getMusic() {
-    const music = { THonly: [], Sisamuth: [], Anime: [], Pop: [], Foreign: [] };
-    
-    for (const dir in music) {
-        const folder = fs.opendirSync('music/' + dir);
-        let files = true;
-
-        while (files) {
-          const file = folder.readSync();
-          if (file) music[dir].push(`${folder.path}/${file.name}`) 
-          else files = false;
-        }
-
-        folder.close();
-    }
-
-    return music;
+async function getInfo(videoID) {
+    const info = await ytdl.getBasicInfo(videoID);
+    return info.videoDetails.title;
 }
 
-//console.log(getMusic());
+function getLibrary() {
+    const library = { THonly: [], Sisamuth: [], Anime: [], Pop: [], Foreign: [] };
+    for (const dir in library) {
+        const folder = fs.opendirSync('music/' + dir);
+        let files = true;
+        while (files) {
+            const file = folder.readSync();
+            if (file) library[dir].push(getData(folder.path, file.name))
+            else files = false;
+        }
+        folder.close();
+    }
+    return library;
+}
+
+// https://stackoverflow.com/questions/10066638/get-youtube-information-via-json-for-single-video-not-feed-in-javascript
+function getData(folder, file) {
+    const info = file.split(' *|* ');
+    const data = {};
+    data.id = info[0];
+    data.title = info[1].replace('.webm', '');
+    data.url = `${folder}/${file}`;
+    data.coverart = `https://i.ytimg.com/vi/${data.id}/maxresdefault.jpg`;
+    data.thumbnail = `https://img.youtube.com/vi/${data.id}/3.jpg`;
+    return data;
+}
+// console.log(getLibrary());

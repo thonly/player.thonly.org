88a3c43e4338100c734cde56f81610df91c45638
Thon Ly <thonly@heartbank.ceo>
2022-07-17 21:37:30 -0700

    v1 complete

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-const video = document.querySelector('video');
-const audio = document.querySelector('audio');
-const categories = ['THonly', 'Sisamuth', 'Anime', 'Pop', 'Foreign'];
+import './timer.mjs';
+import renderPlayer from './player.mjs';
 
 window.onload = async () => {
-    const library = JSON.parse(localStorage.getItem('library'));
-    video.cover = library[categories[0]][0].coverart;
-    video.src = library[categories[0]][0].video;
-    audio.src = library[categories[0]][0].audio;
-    renderMusicLibrary(library);
-}
-
-window.getMusicLibrary = async () => {
-    const library = await fetch('https://dns.thonly.net:432/');
-    localStorage.setItem('library', JSON.stringify(await library.json()));
-    document.location.reload();
-}
-
-window.addMusic = async event => {
-    event.preventDefault();
-    const library = await fetch('https://dns.thonly.net:432/', {
-        method: 'POST', 
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(Object.fromEntries(new FormData(event.target)))
-    });    
-    localStorage.setItem('library', JSON.stringify(await library.json()));
-    document.location.reload();
-}
-
-function renderMusicLibrary(library) {
-    const nav = document.querySelector('nav');
-
-    categories.forEach(category => {
-        const h3 = document.createElement('h3');
-        h3.textContent = category;
-        const menu = document.createElement('menu');
-        nav.append(h3, menu);
-
-        library[category].forEach(song => {
-            const li = document.createElement('li');
-            const img = document.createElement('img');
-            img.src = song.thumbnail;
-            img.onclick = () => window.open(`https://www.youtube.com/watch?v=${song.id}`, '_blank');
-            li.append(img, song.title);
-            li.onclick = () => playMusic(song);
-            menu.append(li);
-        });
-    });
-}
-
-function playMusic(song) {
-    video.cover = song.coverart;
-    video.src = song.video;
-    audio.src = song.audio;
-    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) video.play()
-    else audio.play();
+    renderPlayer();
 }
 
 window.dataLayer = window.dataLayer || [];
---
+++ player.mjs
@@ -0,0 +1,72 @@
+const video = document.querySelector('video');
+const audio = document.querySelector('audio');
+const categories = ['THonly', 'Sisamuth', 'Anime', 'Pop', 'Foreign'];
+
+export default function renderPlayer() {
+    const library = JSON.parse(localStorage.getItem('library'));
+    const favorite = localStorage.getItem('favorite') ? JSON.parse(localStorage.getItem('favorite')) : setFavorite(library[categories[0]][0]);
+    video.cover = favorite.coverart;
+    video.src = favorite.video;
+    audio.src = favorite.audio;
+    renderMusicLibrary(library);
+}
+
+window.getMusicLibrary = async () => {
+    const library = await fetch('https://dns.thonly.net:432/');
+    localStorage.setItem('library', JSON.stringify(await library.json()));
+    document.location.reload();
+}
+
+window.addMusic = async event => {
+    event.preventDefault();
+    const library = await fetch('https://dns.thonly.net:432/', {
+        method: 'POST', 
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(Object.fromEntries(new FormData(event.target)))
+    });    
+    localStorage.setItem('library', JSON.stringify(await library.json()));
+    document.location.reload();
+}
+
+function renderMusicLibrary(library) {
+    const nav = document.querySelector('nav');
+
+    categories.forEach(category => {
+        const h3 = document.createElement('h3');
+        h3.textContent = category;
+        const menu = document.createElement('menu');
+        nav.append(h3, menu);
+
+        library[category].sort(orderByTitle).forEach(song => {
+            const li = document.createElement('li');
+            const img = document.createElement('img');
+            img.src = song.thumbnail;
+            img.onclick = () => window.open(`https://www.youtube.com/watch?v=${song.id}`, '_blank');
+            li.append(img, song.title);
+            li.onclick = () => playMusic(song);
+            menu.append(li);
+        });
+    });
+}
+
+function playMusic(song) {
+    setFavorite(song);
+    video.cover = song.coverart;
+    video.src = song.video;
+    audio.src = song.audio;
+    if (/^((?!chrome|android).)*safari/i.test(navigator.userAgent)) video.play()
+    else audio.play();
+}
+
+function setFavorite(song) {
+    localStorage.setItem('favorite', JSON.stringify(song));
+    return song;
+}
+
+function orderByTitle(a, b) {
+    const titleA = a.title.toUpperCase();
+    const titleB = b.title.toUpperCase();
+    if (titleA < titleB) return -1;
+    if (titleA > titleB) return 1;
+    return 0;
+  }
